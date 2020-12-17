c08e4bcb76c34d6f012689dc38853e372c12efbd
Philipp Gaspar <philipp.gaspar@gmail.com>
2020-12-17 14:19:21 -0300

    Add logger to PCA script.

--- src/pca_model.py
+++ src/pca_model.py
@@ -2,12 +2,14 @@ import sys
 import os
 import pickle
 import glob
+import logging
 
 import tensorflow as tf
 import numpy as np
 import utils as utils
 
 from sklearn.decomposition import PCA
+from datetime import datetime
 
 # Define global variables
 HOME_DIR = os.environ['HOME']
@@ -24,16 +26,22 @@ SEED = 13
 np.random.seed(SEED)
 
 if __name__ == "__main__":
+    # set logger
+    now = datetime.now()
+    date = str(now.strftime('%d.%b.%y'))
+    log_name = 'PCA_%s.log' % (str(date))
+    utils.set_logger(os.path.join(EXPERIMENTS_DIR, 'PCA', log_name))
+
     # -------------------- #
     #    LOAD FILENAMES    #
     # ==================== #
     n_folds = 10
     for fold in range(n_folds):
-        print('Fold #%i' % fold)
+        logging.info('Fold #%i' % fold)
         filename = 'embedding_fold%i.pkl' % fold
         embedding_dir = os.path.join(EXPERIMENTS_DIR, 'Schenzen-Embeddings', 'results')
         input_file = os.path.join(embedding_dir, filename)
-        check_file(input_file)
+        utils.check_file(input_file)
         
         # open dictionary with embeddings
         with open(input_file, 'rb') as handle:
@@ -42,26 +50,26 @@ if __name__ == "__main__":
         # --------------- #
         #    PCA MODEL    #
         # =============== #
-        print('-- PCA MODEL ---')
+        logging.info('-- PCA MODEL ---')
         n_comp = 2
         pca = PCA(n_components=n_comp, random_state=SEED)
-        X_new = pca.fit_transform(embedding['X'])
+        # X_new = pca.fit_transform(embedding['X'])
 
-        variance_ratio = pca.explained_variance_ratio_
-        variance = pca.explained_variance_
+        # variance_ratio = pca.explained_variance_ratio_
+        # variance = pca.explained_variance_
 
-        print('\n - Auxiliary Info to Build SOM')
-        print('   First variance: %1.2f' % variance[0])
-        print('   Second variance: %1.2f' % variance[1])
-        print('   Ratio = %1.2f\n' % (variance[0]/variance[1]))
+        # print('\n - Auxiliary Info to Build SOM')
+        # print('   First variance: %1.2f' % variance[0])
+        # print('   Second variance: %1.2f' % variance[1])
+        # print('   Ratio = %1.2f\n' % (variance[0]/variance[1]))
 
-        print('   First (normalized) variance: %1.2f' % variance_ratio[0])
-        print('   Second (normalized) variance: %1.2f' % variance_ratio[1])
-        print('   Ratio = %1.2f\n' % (variance_ratio[0]/variance_ratio[1]))
+        # print('   First (normalized) variance: %1.2f' % variance_ratio[0])
+        # print('   Second (normalized) variance: %1.2f' % variance_ratio[1])
+        # print('   Ratio = %1.2f\n' % (variance_ratio[0]/variance_ratio[1]))
 
 
-        N = 5 * np.sqrt(len(embedding['y']))
-        print('   N = %1.1f\n' % N)
+        # N = 5 * np.sqrt(len(embedding['y']))
+        # print('   N = %1.1f\n' % N)
 
 
 
