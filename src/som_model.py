589d21f0660157aa809efd18e00630fef2371132
Philipp Gaspar <philipp.gaspar@gmail.com>
2020-12-17 19:23:53 -0300

    Persist results and outputs file.

--- src/som_model.py
+++ src/som_model.py
@@ -14,6 +14,8 @@ import utils as utils
 from minisom import MiniSom
 from sklearn.preprocessing import StandardScaler
 from datetime import datetime
+from collections import Counter, defaultdict
+from itertools import product
 
 # Define global variables
 HOME_DIR = os.environ['HOME']
@@ -73,51 +75,74 @@ if __name__ == "__main__":
         som.pca_weights_init(data)
         som.train(data, num_iteration=1000, verbose=True)
 
-        # -------------------- #
-        #    SAVING OUTPUTS    #
-        # ==================== #
+        
+        # Get winner filenames for each neuron in SOM map
+        winner_files = defaultdict(list)
+        for entry, file in zip(data, embedding['files']):
+            winner_files[som.winner(entry)].append(file)
+
+        # Count the number of events of each class for
+        # each neuron in SOM map
+        map_results = defaultdict(tuple)
+        for position in winner_files:
+            win_names = winner_files[position]
+            n_H0 = len([i for i in win_names if '_0.png' in i])
+            n_H1 = len([i for i in win_names if '_1.png' in i])
+            map_results[position] = (n_H0, n_H1)
+                    
+        # ------------------------------ #
+        #    SAVING RESULTS & OUTPUTS    #
+        # ============================== #
         file_path = os.path.join(OUTPUTS_DIR, 'som_fold%i.pkl' % fold)
         with open(file_path, 'wb') as fp:
             pickle.dump(som, fp)
 
-        # # ---- FIGURE #1 ---- #
-        # colors = ['C3', 'C2']
-        # w_x, w_y = zip(*[som.winner(d) for d in data])
-        # w_x = np.array(w_x)
-        # w_y = np.array(w_y)
+        file_path = os.path.join(RESULTS_DIR, 'winner_files_fold%i.pkl' % fold)
+        with open(file_path, 'wb') as fp:
+            pickle.dump(winner_files, fp)
 
-        # plt.figure(figsize=(12, 6))
-        # plt.pcolor(som.distance_map().T, cmap='bone_r', alpha=0.5)
-        # plt.colorbar()
+        file_path = os.path.join(RESULTS_DIR, 'winner_count_fold%i.pkl' % fold)
+        with open(file_path, 'wb') as fp:
+            pickle.dump(map_results, fp)
 
-        # for c in np.unique(target):
-        #     idx_target = target==c
-        #     plt.scatter(w_x[idx_target]+.5+(np.random.rand(np.sum(idx_target))-.5)*.8,
-        #                 w_y[idx_target]+.5+(np.random.rand(np.sum(idx_target))-.5)*.8, 
-        #                 s=50, c=colors[c-1], label=label_names[c])
-        # plt.legend(loc='upper right', facecolor='w')
-        # plt.grid()
-        # output_file = os.path.join(FIGURES_DIR, 'map1_fold%i.png' % fold)
-        # plt.savefig(output_file, bbox_inches='tight', format='png')
+        # ---- FIGURE #1 ---- #
+        colors = ['C3', 'C2']
+        w_x, w_y = zip(*[som.winner(d) for d in data])
+        w_x = np.array(w_x)
+        w_y = np.array(w_y)
 
-        # labels_map = som.labels_map(data, [label_names[t] for t in target])
+        plt.figure(figsize=(12, 6))
+        plt.pcolor(som.distance_map().T, cmap='bone_r', alpha=0.5)
+        plt.colorbar()
 
-        # # ---- FIGURE #2 ---- #
-        # fig = plt.figure(figsize=(12, 6))
-        # the_grid = gridspec.GridSpec(n_neurons, n_neurons, fig)
+        for c in np.unique(target):
+            idx_target = target==c
+            plt.scatter(w_x[idx_target]+.5+(np.random.rand(np.sum(idx_target))-.5)*.8,
+                        w_y[idx_target]+.5+(np.random.rand(np.sum(idx_target))-.5)*.8, 
+                        s=50, c=colors[c-1], label=label_names[c])
+        plt.legend(loc='upper right', facecolor='w')
+        plt.grid()
+        output_file = os.path.join(FIGURES_DIR, 'map1_fold%i.png' % fold)
+        plt.savefig(output_file, bbox_inches='tight', format='png')
 
-        # for i, j in itertools.product(range(n_neurons), range(m_neurons)):
-        #     if len(labels_map[i, j]) == 0:
-        #         label_frac = [0, 0]
-        #     else:
-        #         label_frac = [labels_map[i,j][l] for l in label_names.values()]
+        labels_map = som.labels_map(data, [label_names[t] for t in target])
+
+        # ---- FIGURE #2 ---- #
+        fig = plt.figure(figsize=(12, 6))
+        the_grid = gridspec.GridSpec(n_neurons, n_neurons, fig)
+
+        for i, j in itertools.product(range(n_neurons), range(m_neurons)):
+            if len(labels_map[i, j]) == 0:
+                label_frac = [0, 0]
+            else:
+                label_frac = [labels_map[i,j][l] for l in label_names.values()]
         
-        #     plt.subplot(the_grid[n_neurons - 1 - j, i], aspect=1)
-        #     patches, texts = plt.pie(label_frac, colors=['C2', 'C3'])
+            plt.subplot(the_grid[n_neurons - 1 - j, i], aspect=1)
+            patches, texts = plt.pie(label_frac, colors=['C2', 'C3'])
 
-        # plt.legend(patches, label_names.values(), bbox_to_anchor=(1.5, 1.5), ncol=3)
-        # output_file = os.path.join(FIGURES_DIR, 'map2_fold%i.png' % fold)
-        # plt.savefig(output_file, bbox_inches='tight', format='png')
+        plt.legend(patches, label_names.values(), bbox_to_anchor=(1.5, 1.5), ncol=3)
+        output_file = os.path.join(FIGURES_DIR, 'map2_fold%i.png' % fold)
+        plt.savefig(output_file, bbox_inches='tight', format='png')
                 
 
 
